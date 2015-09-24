844bb38dd46c055e9883087d63af1a373dfc1f74
yale <yale.spector@gmail.com>
2015-09-24 19:35:24 -0400

    showing blog posts

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ var posts = [{
   body: "Dolor sit amet"
 }];
 
+var BlogPost = React.createClass({
+  render: function() {
+    return <div><h1>{this.props.title}</h1><p>{this.props.body}</p></div>;
+  }
+});
+
 var BlogApp = React.createClass({
   render: function() {
-    return <div>Welcome! I have {this.props.posts.length} posts. Now let's show them!</div>;
+    var posts = this.props.posts.map(function(post) {
+      return <BlogPost {... post} />;
+    });
+
+    return (
+        <div>
+          Welcome! I have {this.props.posts.length} posts. Now let's show them!
+          { posts }
+        </div>
+    );
   }
 });
 

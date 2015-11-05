2322174fa6655caefb65e36125dc58d83e3687e1
yale <yale.spector@gmail.com>
2015-11-05 18:16:18 -0500

    removing posts solution

--- app.js
+++ app.js
@@ -6,22 +6,11 @@ var posts = [{
   body: "Dolor sit amet"
 }];
 
-var BlogPost = React.createClass({
-  render: function() {
-    return <div><h1>{this.props.title}</h1><p>{this.props.body}</p></div>;
-  }
-});
-
 var BlogApp = React.createClass({
   render: function() {
-    var posts = this.props.posts.map(function(post) {
-      return <BlogPost {... post} />;
-    });
-
     return (
         <div>
           Welcome! I have {this.props.posts.length} posts. Now let's show them!
-          { posts }
         </div>
     );
   }
